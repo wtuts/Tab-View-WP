aa6cc98b895b2a8e5506062e5786d396d3234023
vivek maskara <maskaravivek@hotmail.com>
2015-08-23 22:16:11 +0530

    Tab View for Windows Phone 8.1/Windows 10

---
+++ Tab View WP 8.1/Tab View WP 8.1/Converters/IndexToColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Tab_View_WP_8._1.Converters
+{
+    public class IndexToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            int tabIndex = System.Convert.ToInt16(parameter);
+            int selectedIndex = System.Convert.ToInt16(value);
+            if (tabIndex == selectedIndex)
+            {
+                return new SolidColorBrush(Colors.DarkGray);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
